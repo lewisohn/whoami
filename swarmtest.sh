dd711e5d198de7218425b043f1ebb1addddafb7e
Oliver Lewisohn <ollie.jl+github@gmail.com>
2020-05-08 15:24:28 +0300

    Cleaning up

--- swarmtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..10}
-do
-    curl -H Host:whoami.docker.localhost http://127.0.0.1:5000/api/whoami
-    echo
-done
--- teardown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kubectl drain minikube --ignore-daemonsets --delete-local-data --force
-kubectl delete deployment --all
-kubectl delete svc --all
-kubectl delete rs --all
-kubectl delete rc --all
-kubectl uncordon minikube

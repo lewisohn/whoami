1cb5b2b75e73ea690a93235f8888f136c8d0b3e9
Oliver Lewisohn <ollie.jl+github@gmail.com>
2020-05-13 16:40:03 +0300

    Concourse

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install pipenv
 pipenv install --dev
-pipenv run python -m pytest test_app.py
-pipenv run python -m pytest ./lib/test_whoami.py
+echo pwd
+pipenv run python -m pytest

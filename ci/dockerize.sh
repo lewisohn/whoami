207c66547950d11bcedd93bd19d0e241e0a53b2d
Oliver Lewisohn <ollie.jl+github@gmail.com>
2020-05-14 14:10:13 +0300

    Concourse

--- ci/dockerize.sh
+++ ci/dockerize.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-make build push
+pwd
+docker build -t lewisohn/whoami:0.1.6
+docker push lewisohn/whoami:0.1.6
+docker tag lewisohn/whoami:0.1.6 lewisohn/whoami:latest
+docker push lewisohn/whoami:latest
